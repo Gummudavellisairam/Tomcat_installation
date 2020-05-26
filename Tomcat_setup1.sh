2df2dc49d9265feda8c9b1ce0e221e097ffbc046
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 16:06:11 +0530

    Update Tomcat_setup1.sh

--- Tomcat_setup1.sh
+++ Tomcat_setup1.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-          yum install java-1.8.0
-         echo $(wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.35/bin/apache-tomcat-9.0.35.tar.gz)
+         
+          if [ $? = 0 ]
+          then
+             echo " Java installed successfully"
+          else
+             echo " Unable to install java"
+          fi
+        
            echo $(tar -zxpvf apache-tomcat-9.0.35.tar.gz)
              echo $(echo "export CATALINA_HOME=apache-tomcat-9.0.33" >> /$HOME/.bashrc)
              echo $(echo $(source ~/.bashrc))
