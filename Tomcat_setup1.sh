9f4a0dd3a001a6e12f62d915f9f817a762b70d01
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 15:56:26 +0530

    Update Tomcat_setup1.sh

--- Tomcat_setup1.sh
+++ Tomcat_setup1.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
           yum install java-1.8.0
-          
-        #echo $(wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.35/bin/apache-tomcat-9.0.35.tar.gz)
-          # echo $(tar -zxpvf apache-tomcat-9.0.35.tar.gz)
+         echo $(wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.35/bin/apache-tomcat-9.0.35.tar.gz)
+           echo $(tar -zxpvf apache-tomcat-9.0.35.tar.gz)
              echo $(echo "export CATALINA_HOME=apache-tomcat-9.0.33" >> /$HOME/.bashrc)
              echo $(echo $(source ~/.bashrc))
-         
              echo $(firewall-cmd --permanent --zone=public --add-port=8080/tcp)
              echo $(firewall-cmd --reload)
-
