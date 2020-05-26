6feb65d9b007e39d702a2f1492b3f1680c3cd15c
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 15:29:42 +0530

    Update Tomcat_setup1.sh

--- Tomcat_setup1.sh
+++ Tomcat_setup1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-function java_installation()
-{
+
           yum install java-1.8.0
           if [ $? = 0 ]
           then
@@ -10,9 +9,6 @@ function java_installation()
              echo " Unable to install java"
           fi
 
-}
-function tomcat_installation()
-{
              wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz
              tar -zxpvf apache-tomcat-9.0.33.tar.gz
              echo "export CATALINA_HOME=apache-tomcat-9.0.33" >> /$HOME/.bashrc
@@ -24,6 +20,3 @@ function tomcat_installation()
              firewall-cmd --permanent --zone=public --add-port=8080/tcp
              firewall-cmd --reload
 
-}
-java_installation
-tomcat_installation
