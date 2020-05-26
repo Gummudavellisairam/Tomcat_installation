b0756055f9c5db917e5dd326e604a922456aa706
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 12:49:05 +0530

    Update create_installer.sh

--- create_installer.sh
+++ create_installer.sh
@@ -3,4 +3,4 @@ mkdir $HOME/tomcat_practice5
 cp Tomcat_setup1.sh $HOME/tomcat_practice5
 cp Java_install.sh $HOME/tomcat_practice5
 cp Tomcat_install.sh $HOME/tomcat_practice5
-makeself.sh /$HOME/tomcat_practice5 ./tomcat-install.run "Tomcat installing" ./Tomcat_setup1.sh
+makeself /$HOME/tomcat_practice5? ./tomcat-install.run "Tomcat installing" ./Tomcat_setup1.sh
