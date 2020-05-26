ccb302133e59a2b5fd5d5da41059b38cddf8c0e4
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 15:53:10 +0530

    Update Tomcat_setup1.sh

--- Tomcat_setup1.sh
+++ Tomcat_setup1.sh
@@ -3,8 +3,8 @@
           
         #echo $(wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.35/bin/apache-tomcat-9.0.35.tar.gz)
           # echo $(tar -zxpvf apache-tomcat-9.0.35.tar.gz)
-             #echo "export CATALINA_HOME=apache-tomcat-9.0.33" >> /$HOME/.bashrc
-             #echo $(source ~/.bashrc)
+             echo $(echo "export CATALINA_HOME=apache-tomcat-9.0.33" >> /$HOME/.bashrc)
+             echo $(echo $(source ~/.bashrc))
          
              echo $(firewall-cmd --permanent --zone=public --add-port=8080/tcp)
              echo $(firewall-cmd --reload)
