40ecb5d2ecc09619167ce34f50fb9bd9c2dfc40b
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 15:27:45 +0530

    Update Tomcat_setup1.sh

--- Tomcat_setup1.sh
+++ Tomcat_setup1.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+function java_installation()
+{
+          yum install java-1.8.0
+          if [ $? = 0 ]
+          then
+             echo " Java installed successfully"
+          else
+             echo " Unable to install java"
+          fi
+
+}
 function tomcat_installation()
 {
              wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz
@@ -11,17 +23,6 @@ function tomcat_installation()
              }
              firewall-cmd --permanent --zone=public --add-port=8080/tcp
              firewall-cmd --reload
-
-}
-function java_installation()
-{
-          yum install java-1.8.0
-          if [ $? = 0 ]
-          then
-             echo " Java installed successfully"
-          else
-             echo " Unable to install java"
-          fi
 
 }
 java_installation
